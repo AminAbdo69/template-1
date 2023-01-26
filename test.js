1593e8508e8be90b3c10b77f3a8079fa9911f3bc
AminAbdo <AminAbdo69>
2023-01-26 21:23:14 +0200

    addddd

---
+++ test.js
@@ -0,0 +1,6 @@
+
+document.querySelector(".icon").onclick = function() {
+    document.querySelector("ul").style.display="block"; }
+
+    document.querySelector(".landing"  ).onclick = function() {
+        document.querySelector("ul").style.display="none"; }
